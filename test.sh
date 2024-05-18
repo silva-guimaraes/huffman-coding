4c86303267e4ea532213b429b750b7343faddbc4
silva guimaraes <silva__guimaraes@hotmail.com>
2024-05-18 00:51:56 -0300

    adicionar testes

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+set -e
+
+./build.sh
+
+echo 'biblia versão king james: "biblia"'
+./main < ./input/biblia.txt
+./main ./output.huff > foobar
+diff ./input/biblia.txt foobar
+
+printf '\n'
+echo 'documentação do ffmpeg: "teste.txt"'
+./main < ./input/teste.txt
+./main ./output.huff > foobar
+diff ./input/teste.txt foobar
+
+printf '\n'
+echo 'a.txt: "a.txt.txt"'
+./main < ./input/a.txt
+./main ./output.huff > foobar
+diff ./input/rance10_script.txt foobar
+
+printf '\n'
+echo 'rance X: "rance10_script.txt"'
+./main < ./input/rance10_script.txt
+./main ./output.huff > foobar
+diff ./input/rance10_script.txt foobar
