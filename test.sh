9774410dcafc39542ab62a4a971c66cf246039e2
silva guimaraes <silva__guimaraes@hotmail.com>
2024-05-19 12:17:23 -0300

    adicionar mais testes ao script

--- test.sh
+++ test.sh
@@ -12,6 +12,8 @@ test () {
     printf '\ndecodificação:\n'
     time ./main ./output.huff > foobar
     echo
+    wc -l $2
+    wc -c $2
     du -h --apparent-size $2
     du -h --apparent-size ./output.huff
     du -h --apparent-size foobar
@@ -19,9 +21,12 @@ test () {
     echo
 }
 
-test 'biblia versão king james: "biblia"' ./input/biblia.txt
+test 'huffman_wiki.txt' ./input/huffman_wiki.txt
 
 test 'documentação do ffmpeg: "ffmpeg_doc.txt"' ./input/ffmpeg_doc.txt
+
+test 'biblia versão king james: "biblia"' ./input/biblia.txt
+
 
 test 'a.txt: "a.txt"' ./input/a.txt
 
